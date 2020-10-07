bdf798fe387f52652a8c44d991e0bcaf31dea757
wujiqiang11 <931556930@qq.com>
2020-10-07 20:57:31 +0800

    添加项目文件。

---
+++ easyClient/client.cpp
@@ -0,0 +1,53 @@
+#define WIN32_LEAN_AND_MEAN
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
+#pragma comment(lib,"ws2_32.lib")
+#include<Windows.h>
+#include<WinSock2.h>
+#include <iostream>
+
+using namespace std;
+int main()
+{
+	//启动windows socket 2.x环境
+	WORD ver = MAKEWORD(2, 2);  // 算出版本号
+	WSADATA dat;
+	WSAStartup(ver, &dat);
+	//----------------
+	//--- 使用socket API建立一个简易的TCP客户端
+	//  1.建立一个socket
+	SOCKET _csock = socket(AF_INET, SOCK_STREAM, 0);
+	if (_csock == INVALID_SOCKET)
+		printf("建立Socket失败\n");
+	else
+	{
+		printf("建立Socket成功..\n");
+	}
+	//  2.连接服务器 connect
+	sockaddr_in _sin = {};
+	_sin.sin_family = AF_INET;
+	_sin.sin_port = htons(4567);
+	_sin.sin_addr.S_un.S_addr = inet_addr("127.0.0.1");
+	int net = connect(_csock, (sockaddr*)&_sin, sizeof(sockaddr_in));
+	if (net == INVALID_SOCKET)
+	{
+		printf("ERROR 连接失败！\n");
+	}
+	else
+	{
+		printf("连接成功\n");
+	}
+	//  3.接受服务器信息 revc
+	char recBuf[256] = {};
+	int nlen = recv(_csock, recBuf, 256, 0);
+	if (nlen > 0)
+	{
+		printf("接收到数据:%s\n", recBuf);
+	}
+	//  4.关闭socket closesocket
+	closesocket(_csock);
+	//----------------
+	//清除windows socket环境
+	WSACleanup();
+	getchar();
+	return 0;
+}
---
+++ easyServer/server.cpp
@@ -0,0 +1,66 @@
+#define WIN32_LEAN_AND_MEAN
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
+#pragma comment(lib,"ws2_32.lib")
+#include<Windows.h>
+#include<WinSock2.h>
+#include <iostream>
+
+using namespace std;
+int main()
+{
+	//启动windows socket 2.x环境
+	WORD ver = MAKEWORD(2, 2);  // 算出版本号
+	WSADATA dat;
+	WSAStartup(ver, &dat);
+	//----------------
+	//--- 使用socket API建立简易TCP服务端
+	//  1.建立一个socket
+	SOCKET _sock =socket (AF_INET, SOCK_STREAM, IPPROTO_TCP);
+	//  2.bind 绑定用于接受客户端连接的网络端口
+	sockaddr_in _sin = {};
+	_sin.sin_family = AF_INET;  //socket protocol
+	_sin.sin_port = htons(4567);  //host to net unsighned short
+	_sin.sin_addr.S_un.S_addr = INADDR_ANY;// inet_addr("127.0.0.1");
+	if (SOCKET_ERROR == bind(_sock, (sockaddr*)&_sin, sizeof(_sin)))
+	{
+		printf("ERROR, 服务端绑定端口失败！\n");
+	}
+	printf("服务端绑定端口成功！\n");
+	//  3.listen 监听网络端口
+	if (SOCKET_ERROR == listen(_sock, 5))
+	{
+		printf("错误，监听网络端口失败..\n");
+	}
+	else
+	{
+		printf("监听网络端口成功\n");
+	}
+	//  4.accept 等待接受客户端连接
+	sockaddr_in clientAdd = {};
+	int AddLen = sizeof(sockaddr_in);
+	SOCKET _csock = INVALID_SOCKET;
+	char msgbuf[] = "Helow, I`m server!";
+	while (true)
+	{
+		_csock = accept(_sock, (sockaddr*)&clientAdd, &AddLen);
+		if (_csock == INVALID_SOCKET)
+		{
+			printf("接收到无效 socket\n");
+		}
+		printf("新的客户端加入, IP = %s \n", inet_ntoa(clientAdd.sin_addr));
+		//  5.send 向客户端发送一条数据
+		send(_csock, msgbuf, strlen(msgbuf) + 1, 0);
+	}
+
+
+	//  6.关闭socket closesocket
+	closesocket(_sock);
+	//------------------
+	//清除windows socket环境
+	WSACleanup();
+	return 0;
+
+}
+
+
+
