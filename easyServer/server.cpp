2549315b716cd5e65da753ec1e1ac31d249ae24f
wujiqiang <wujiqiang@LAPTOP-AL8RCRRK>
2020-10-13 19:49:46 +0800

    server select 更改0.5

--- easyServer/server.cpp
+++ easyServer/server.cpp
@@ -4,6 +4,7 @@
 #include<Windows.h>
 #include<WinSock2.h>
 #include <iostream>
+#include<vector>
 
 using namespace std;
 enum CMD
@@ -57,6 +58,60 @@ struct LogOutResult :public pkgHeader
 	}
 	int result;
 };
+
+std::vector<SOCKET> g_clients;
+
+int process(SOCKET  _csock)
+{
+	char recBuf[1024];  //接收缓存区
+	pkgHeader* recHeader;
+	int recBufLen = recv(_csock, recBuf, sizeof(pkgHeader), 0);  //接收消息包头
+	if (recBufLen <= 0)
+	{
+		return -1;
+	}
+	else
+	{
+		recHeader = (pkgHeader*)recBuf;
+		printf("接收到包头: 长度：%d,类型：%d\n", recHeader->pkgLen, recHeader->cmd);
+	}
+	switch (recHeader->cmd)  //查看包头的命令类型
+	{
+	case(CMD_LOGIN):
+	{
+		recBufLen = recv(_csock, recBuf + sizeof(pkgHeader), sizeof(LoginData) - sizeof(pkgHeader), 0);
+		LoginData* loginMse = (LoginData*)recBuf;
+		if (recBufLen > 0)
+		{
+			printf("接收到登录信息,用户名：%s, 密码：%s\n", loginMse->userName, loginMse->userWord);
+			LoginResult loginReMse;
+			loginReMse.result = 202;
+			send(_csock, (const char*)&loginReMse, sizeof(LoginResult), 0);
+		}
+	}
+	break;
+	case(CMD_LOGOUT):
+	{
+		recBufLen = recv(_csock, recBuf + sizeof(pkgHeader), sizeof(LogOutData) - sizeof(pkgHeader), 0);
+		LogOutData* logoutMse = (LogOutData*)recBuf;
+		if (recBufLen > 0)
+		{
+			printf("接收到登出信息,用户名：%s\n", logoutMse->userName);
+			LogOutResult logoutReMse;
+			logoutReMse.result = 202;
+			send(_csock, (const char*)&logoutReMse, sizeof(LogOutResult), 0);
+		}
+	}
+	break;
+	default:
+		pkgHeader header = {};
+		header.cmd = CMD_ERROR;
+		header.pkgLen = 0;
+		send(_csock, (const char*)&header, sizeof(pkgHeader), 0);
+		break;
+	}
+}
+
 int main()
 {
 	//启动windows socket 2.x环境
@@ -86,69 +141,74 @@ int main()
 	{
 		printf("监听网络端口成功\n");
 	}
-	//  4.accept 等待接受客户端连接
-	sockaddr_in clientAdd = {};
-	int AddLen = sizeof(sockaddr_in);
-	SOCKET _csock = INVALID_SOCKET;
 	
-	_csock = accept(_sock, (sockaddr*)&clientAdd, &AddLen);
-	if (_csock == INVALID_SOCKET)
-	{
-		printf("接收到无效 socket\n");
-	}
-	printf("新的客户端加入, IP = %s \n", inet_ntoa(clientAdd.sin_addr));
+	
 
 	while (true)
 	{
-		char recBuf[1024];  //接收缓存区
-		pkgHeader* recHeader;
-		int recBufLen = recv(_csock, recBuf, sizeof(pkgHeader), 0);  //接收消息包头
-		if (recBufLen <= 0)
+		fd_set fdRead;
+		fd_set fdWrite;
+		fd_set fdExp;
+
+		FD_ZERO(&fdRead);
+		FD_ZERO(&fdWrite);
+		FD_ZERO(&fdExp);
+
+		FD_SET(_sock, &fdRead);
+		FD_SET(_sock, &fdWrite);
+		FD_SET(_sock, &fdExp);
+
+		for (int n = (int)g_clients.size()-1; n >= 0; n--)
 		{
-			printf("客户端退出\n");
+			cout << n << endl;
+			FD_SET(g_clients[n], &fdRead);
+		}
+
+		//nfds  是集合fd_set中最后一个socket描述符+1, 用以表示集合范围
+		int ret = select(_sock + 1, &fdRead, &fdWrite, &fdExp, NULL);  //select将更新这个集合,把其中不可读(不可写)的套节字去掉 
+		if (ret < 0)
+		{
+			printf("select 任务结束.\n");
 			break;
 		}
-		else
+		if (FD_ISSET(_sock, &fdRead))  //查看_sock是否还有可读请求
 		{
-			recHeader = (pkgHeader*)recBuf;
-			printf("接收到包头: 长度：%d,类型：%d\n", recHeader->pkgLen,recHeader->cmd);
-		}
-		switch (recHeader->cmd)  //查看包头的命令类型
-		{
-			case(CMD_LOGIN):
+			FD_CLR(_sock, &fdRead);
+			//  4.accept 等待接受客户端连接
+			sockaddr_in clientAdd = {};
+			int AddLen = sizeof(sockaddr_in);
+			SOCKET _csock = INVALID_SOCKET;
+			_csock = accept(_sock, (sockaddr*)&clientAdd, &AddLen);
+			if (_csock == INVALID_SOCKET)
 			{
-				recBufLen = recv(_csock, recBuf+sizeof(pkgHeader), sizeof(LoginData)-sizeof(pkgHeader), 0);
-				LoginData* loginMse = (LoginData*)recBuf;
-				if (recBufLen > 0)
+				printf("接收到无效 socket\n");
+			}
+			g_clients.push_back(_csock);
+			printf("新的客户端加入, IP = %s \n", inet_ntoa(clientAdd.sin_addr));
+
+			for (int i = 0; i < fdRead.fd_count; i++)
+			{
+				if (process(fdRead.fd_array[i]) == -1)
 				{
-					printf("接收到登录信息,用户名：%s, 密码：%s\n", loginMse->userName, loginMse->userWord);
-					LoginResult loginReMse;
-					loginReMse.result = 202;
-					send(_csock, (const char*)&loginReMse, sizeof(LoginResult), 0);
+					auto iter = find(g_clients.begin(), g_clients.end(), fdRead.fd_array[i]);
+					if (iter != g_clients.end())
+					{
+						g_clients.erase(iter);
+					}
+					printf("客户端退出.\n");
+					
 				}
 			}
-				break;
-			case(CMD_LOGOUT):
-			{
-				recBufLen = recv(_csock, recBuf + sizeof(pkgHeader), sizeof(LogOutData) - sizeof(pkgHeader), 0);
-				LogOutData* logoutMse = (LogOutData*)recBuf;
-				if (recBufLen > 0)
-				{
-					printf("接收到登出信息,用户名：%s\n", logoutMse->userName);
-					LogOutResult logoutReMse;
-					logoutReMse.result = 202;
-					send(_csock, (const char*)&logoutReMse, sizeof(LogOutResult), 0);
-				}
-			}
-				break;
-			default:
-				pkgHeader header = {};
-				header.cmd = CMD_ERROR;
-				header.pkgLen = 0;
-				send(_csock, (const char*)&header, sizeof(pkgHeader), 0);
-				break;
+
 		}
+
 	}
+
+	for (size_t n = 0; n < g_clients.size(); n++)
+	{
+		closesocket(g_clients[n]);
+	}
+
 	//  6.关闭socket closesocket
 	closesocket(_sock);
 	//------------------
