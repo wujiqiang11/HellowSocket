fb36bafd0096dc7623fadbe518ac40c74cf84fbb
wujiqiang <wujiqiang@LAPTOP-AL8RCRRK>
2020-10-16 17:31:08 +0800

    server 封装到一半

---
+++ easyServer/Message.hpp
@@ -0,0 +1,77 @@
+#ifndef _MESSAGE_
+#define _MESSAGE_
+
+
+enum CMD
+{
+	CMD_LOGIN,  // 登录消息
+	CMD_LOGINRE,  // 登陆返回消息
+	CMD_LOGOUT,  // 登出消息
+	CMD_LOGOUTRE,  // 登出返回消息
+	CMD_ERROR,  //错误消息
+	CMD_LOGIN_BRO,  // 登录广播
+	CMD_LOGOUT_BRO  // 登出广播
+};
+struct pkgHeader
+{
+	short pkgLen;
+	CMD cmd;
+};
+
+struct LoginData :public pkgHeader
+{
+	LoginData()
+	{
+		pkgLen = sizeof(LoginData);
+		cmd = CMD_LOGIN;
+	}
+	char userName[32];
+	char userWord[32];
+};
+struct LoginResult :public pkgHeader
+{
+	LoginResult()
+	{
+		pkgLen = sizeof(LoginResult);
+		cmd = CMD_LOGINRE;
+	}
+	int result;
+};
+struct LogOutData :public pkgHeader
+{
+	LogOutData()
+	{
+		pkgLen = sizeof(LogOutData);
+		cmd = CMD_LOGOUT;
+	}
+	char userName[32];
+};
+struct LogOutResult :public pkgHeader
+{
+	LogOutResult()
+	{
+		pkgLen = sizeof(LogOutResult);
+		cmd = CMD_LOGOUTRE;
+	}
+	int result;
+};
+struct LoginBro :public pkgHeader
+{
+	LoginBro()
+	{
+		pkgLen = sizeof(LoginBro);
+		cmd = CMD_LOGIN_BRO;
+	}
+	char userID[32];
+};
+struct LogoutBro :public pkgHeader
+{
+	LogoutBro()
+	{
+		pkgLen = sizeof(LogoutBro);
+		cmd = CMD_LOGOUT_BRO;
+	}
+	char userID[32];
+};
+
+#endif // !_MESSAGE_
