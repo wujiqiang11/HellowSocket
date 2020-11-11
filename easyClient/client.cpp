4c2b1f51dc69fe0ec0c55e08dfa23255a9d5120f
wujiqiang <wujiqiang@LAPTOP-AL8RCRRK>
2020-11-11 08:32:09 +0800

    客户端多线程发送数据

--- easyClient/client.cpp
+++ easyClient/client.cpp
@@ -20,30 +20,43 @@ void inputCMD()
 	}
 }
 
-int main()
+const int client_num = 1000;
+TcpClient* client[client_num];
+int Thread_num = 4;  //客户端开启的发送线程数
+
+void SendThread(int n, int num)
 {
-	//const int client_num = FD_SETSIZE - 1;
-	const int client_num = 1000;
-	TcpClient* client[client_num];
-	for (int i = 0; i < client_num; i++)
+	for (int i = n*num; i < (n+1)*num; i++)
 	{
 		client[i] = new TcpClient();
 		//client[i]->Connect("172.18.166.60", 4567);
 		client[i]->Connect("127.0.0.1", 4567);
 	}
-	std::thread t(inputCMD);
-	t.detach();
 	while (keep_running)
 	{
-		for (int i = 0; i < client_num; i++)
+		for (int i = n * num; i < (n + 1) * num; i++)
 		{
 			client[i]->SendTest();
 			client[i]->KeepRun();
 		}
 	}
-	for (int i = 0; i < client_num; i++)
+	for (int i = n * num; i < (n + 1) * num; i++)
 	{
-		delete [] client[i];
+		delete[] client[i];
+	}
+}
+
+
+
+int main()
+{
+	std::thread t(inputCMD);
+	t.detach();
+
+	for (int i = 0; i < Thread_num; i++)
+	{
+		std::thread t(SendThread, i, client_num / Thread_num);
+		t.detach();
 	}
 	//client.Connect("127.0.0.1", 4567);
 	/*
